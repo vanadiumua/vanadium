f50105c5a0d0502ecabec7c2816d1a291d5bbe24
Sejin Kim <kim3@kenyon.edu>
2020-06-21 16:27:30 -0400

    Update devcopy.sh

--- devcopy.sh
+++ devcopy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Define final directory variable
-FINAL_DIR=brydon1
-
+# Grab the username
+echo "Please enter the username for /var/www/html/: "
+read -p "Enter: " FINAL_DIR
 # Inform user copying will occur
 echo ""
 echo "Attempting to copy HTML and PHP files to /var/www/html/"$FINAL_DIR

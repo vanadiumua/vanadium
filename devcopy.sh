bfef8fc74dd4ef984b52690a0a7abf8cc57133cd
root <root@peanuts.ig2p5f314adexavoy1yxufbn5h.bx.internal.cloudapp.net>
2020-06-21 19:37:27 +0000

    Modified .sh files

--- devcopy.sh
+++ devcopy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Attempting to copy HTML and PHP files to /var/www/html"
+# Define final directory variable
+FINAL_DIR=brydon1
+
+# Inform user copying will occur
+echo ""
+echo "Attempting to copy HTML and PHP files to /var/www/html/"$FINAL_DIR
+echo ""
+
+sudo cp -r ~/vanadium/web/. /var/www/html/$FINAL_DIR
+

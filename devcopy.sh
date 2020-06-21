3ea890b59803f974c546c52a9c9c9d5c936c1637
Sejin Kim <kim3@kenyon.edu>
2020-06-21 16:29:46 -0400

    :memo: Change FINAL_DIR variable

--- devcopy.sh
+++ devcopy.sh
@@ -3,6 +3,7 @@
 # Grab the username
 echo "Please enter the username for /var/www/html/: "
 read -p "Enter: " FINAL_DIR
+
 # Inform user copying will occur
 echo ""
 echo "Attempting to copy HTML and PHP files to /var/www/html/"$FINAL_DIR
